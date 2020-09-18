63a19160b0bba6ae94a0cc6c3e4124334971c53e
slavah8 <vhq5091@psu.edu>
2020-09-18 21:50:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def digit_sum(n):
+  if n == 0:
+    return n;
+  else:
+    return (n % 10 + digit_sum(n//10));
+
+def run():
+  askInt = int(input("Enter an int: "))
+  print(f"sum of digits {askInt} is {digit_sum(askInt)}.")
+
+if __name__ == "__main__": 
+  run()
